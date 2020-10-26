7f33f30de31d5b00c55b577b86f96669e287c765
lorenzo-damico <loredam@hotmail.it>
2020-10-26 17:41:59 +0100

    Creazione model, controller, metodo index e route.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Studenti</title>
+  </head>
+  <body>
+    <h1>Studenti</h1>
+  </body>
+</html>
